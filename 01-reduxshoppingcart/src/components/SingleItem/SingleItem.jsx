f3b6aebac59747b0c251f6ba7f70ed476327058d
Chimoha31 <mokochii1108@gmail.com>
2022-07-09 15:30:08 -0700

    Created Redux folder

--- 01-reduxshoppingcart/src/components/SingleItem/SingleItem.jsx
+++ 01-reduxshoppingcart/src/components/SingleItem/SingleItem.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import styles from "./SingleItem.module.css";
 
 const SingleItem = () => {
   return (
-    <div>
-      Single Item
-    </div>
-  )
-}
+    <div className={styles.singleItem}>
+      <img
+        className={styles.singleItem__image}
+        src="https://images-na.ssl-images-amazon.com/images/I/81wesMu59KL._AC._SR360,460.jpg"
+        alt="Item"
+      />
+      <div className={styles.singleItem__details}>
+        <p className={styles.details__title}>Dog T-shirt</p>
+        <p className={styles.details__description}>This is very Cute t-shirt!</p>
+        <p className={styles.details__price}>$ 22.30</p>
 
-export default SingleItem
+        <button className={styles.details__addBtn}>Add To Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleItem;
