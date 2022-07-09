371685506cc39e3a7342113f74e1c77ea14f56ec
Chimoha31 <mokochii1108@gmail.com>
2022-07-09 15:05:40 -0700

    Added css module

--- 01-reduxshoppingcart/src/App.js
+++ 01-reduxshoppingcart/src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
+import SingleItem from "./components/SingleItem/SingleItem";
 
 const App = () => {
-  return <div>App.</div>;
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Products />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/product/:id" element={<SingleItem />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
